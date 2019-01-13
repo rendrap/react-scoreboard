16babd53d518813519e26dd27309b34967256e9a
rendrap <rendrapradana@gmail.com>
2019-01-13 19:12:51 +0700

    render header in DOM

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const title = React.createElement(
   'h1',
-  { id: 'main-title'},
-  'My first React Element LOL!'
-  );
+  { id: 'main-title' },
+  'My first React Element!'
+);
+
+const desc = React.createElement(
+  'p',
+  null,
+  'I just learned to create a React node and render it into the DOM'
+);
+
+const header = React.createElement(
+  'header',
+  null,
+  title,
+  desc
+);
 
 ReactDOM.render(
-  title,
+  header,
   document.getElementById('root'));
 
-// console.log(title);
+// console.log(title);3
