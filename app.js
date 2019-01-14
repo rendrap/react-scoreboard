98c85567bffc4688e8dbb531e0ec0d089e592f13
rendrap <rendrapradana@gmail.com>
2019-01-14 19:13:44 +0700

    change to component

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-const desc = 'I just learned how to create a React node';
-const myTitleID = 'main-title';
-const name = 'Rendra';
+function Header () {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+    );
 
-const header = (
-  <header>
-    <h1 id={ myTitleID }>{ name }'s First React Element!</h1>
-    <p>{ desc }</p>
-  </header>
-
-);
+}
 
 ReactDOM.render(
-  header,
-  document.getElementById('root'));
-
-// console.log(title);3
+  <Header />,
+  document.getElementById('root')
+);
